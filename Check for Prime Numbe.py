ad23eb16d53fe9ef0fbaa1db37330afe3105d871
yyccPhil <112043923+yyccPhil@users.noreply.github.com>
2022-10-25 23:21:00 -0500

    Update and rename Check for Prime Numbe.pyrs to Check for Prime Numbe.py

--- Check for Prime Numbe.py
+++ Check for Prime Numbe.py
@@ -4,15 +4,15 @@ check_prime = [26, 39, 51, 53, 57, 79, 85]
 ## write your code here
 ## HINT: You can use the modulo operator to find a factor
 
-cnt = 0
-for num in check_prime:
-    for i in range(2, num):
-        if num % i == 0:
-            print("{} is NOT a prime number, because {} is a factor of {}".format(num, i ,num))
-            cnt += 1
-            break
-    if cnt == 0:
-        print("{} IS a prime number.".format(num))
+# cnt = 0
+# for num in check_prime:
+#     for i in range(2, num):
+#         if num % i == 0:
+#             print("{} is NOT a prime number, because {} is a factor of {}".format(num, i ,num))
+#             cnt += 1
+#             break
+#     if cnt == 0:
+#         print("{} IS a prime number.".format(num))
 
 
 
@@ -27,6 +27,7 @@ for num in check_prime:
             print("{} is NOT a prime number, because {} is a factor of {}".format(num, i, num))
             break
 
+# that's best!!!
 # otherwise keep checking until we've searched all possible factors, and then declare it prime
         if i == num -1:    
             print("{} IS a prime number".format(num))
