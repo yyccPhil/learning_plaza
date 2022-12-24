643fff9e36ca40e5f30a9809985cd8bb5acce7d0
Yuan Yuan <112043923+yyccPhil@users.noreply.github.com>
2022-12-24 16:49:48 -0600

    Update Triangle_Quest_2.py

--- Triangle_Quest_2.py
+++ Triangle_Quest_2.py
@@ -1,1 +1,2 @@
-for 
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print (*range(1, i + 1), *range(i - 1, 0, -1), sep = __name__[:0])
