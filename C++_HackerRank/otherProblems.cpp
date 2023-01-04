06ec1664254f212befe86cc1986850040562f9f2
Yuan Yuan <112043923+yyccPhil@users.noreply.github.com>
2023-01-04 10:09:40 -0600

    my first cpp HackerRank solution, lol

---
+++ C++_HackerRank/otherProblems.cpp
@@ -0,0 +1,11 @@
+// Input and Output
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+    return 0;
+}
+
+// -----------------------------------------------------------------------------------------------
+
