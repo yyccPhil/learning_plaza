bcaf785ab6f9096b4490c608ef5b6476f9a36654
Yuan Yuan <112043923+yyccPhil@users.noreply.github.com>
2023-01-10 21:28:20 -0600

    Update otherProblems.cpp

--- C++_HackerRank/otherProblems.cpp
+++ C++_HackerRank/otherProblems.cpp
@@ -9,3 +9,27 @@ int main() {
 
 // -----------------------------------------------------------------------------------------------
 
+// Basic Data Types
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    
+    cin >> a >> b >> c >> d >>e;
+    
+    cout << a << endl
+         << b << endl
+         << c << endl
+         << fixed << setprecision(3) << d << endl
+         << setprecision(9) << e;
+    
+    return 0;
+}
