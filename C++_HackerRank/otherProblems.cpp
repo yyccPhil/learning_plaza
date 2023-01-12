98cabbe1be4c33d1d8fd4481163d07a70fa4a396
Yuan Yuan <112043923+yyccPhil@users.noreply.github.com>
2023-01-11 23:25:17 -0600

    Update otherProblems.cpp

--- C++_HackerRank/otherProblems.cpp
+++ C++_HackerRank/otherProblems.cpp
@@ -33,3 +33,21 @@ int main() {
     
     return 0;
 }
+
+// -----------------------------------------------------------------------------------------------
+
+// For Loop
+int main() {
+    // Complete the code.
+    int first, last;
+    string numbers[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    cin >> first >> last;
+    for (int i = first; i <= last; i++) {
+        if (i <= 9) cout << numbers[i - 1] << endl;
+        else {
+            if (i % 2 == 0) cout << "even" << endl;
+            else cout << "odd" << endl;
+        }
+    }
+    return 0;
+}
