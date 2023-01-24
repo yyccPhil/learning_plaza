3a3a68025560758eac466e7dd4346cc135b6d99d
Yuan Yuan <112043923+yyccPhil@users.noreply.github.com>
2023-01-24 13:55:19 -0600

    Update other_query.sql

--- SQL_HackerRank/other_query.sql
+++ SQL_HackerRank/other_query.sql
@@ -111,3 +111,13 @@ LIMIT 1
 SELECT truncate(sum(LAT_N), 4)
 FROM STATION
 WHERE LAT_N < 137.2345 AND LAT_N > 38.788
+
+------------------------------------------------------------------------------------
+
+-- Weather Observation Station 14
+SELECT truncate(LAT_N, 4)
+FROM STATION
+WHERE LAT_N < 137.2345
+ORDER BY LAT_N DESC
+LIMIT 1
+
