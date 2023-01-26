f1fdb2a42268e5717a01ddc50acd40a94dec0ddb
Yuan Yuan <112043923+yyccPhil@users.noreply.github.com>
2023-01-26 14:36:19 -0600

    Create uda_DSA.py

---
+++ courses/uda_DSA.py
@@ -0,0 +1,9 @@
+# Introduction and Efficiency
+
+## 6. Python: The Basics
+def show_excitement():
+    # Your code goes here!
+    a = ""
+    for i in range(5):
+        a += "I am super excited for this course! "
+    return a.strip()
