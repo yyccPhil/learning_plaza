569d059f697c874b067c11e4eb9a9c3f67984e78
Yuan Yuan <112043923+yyccPhil@users.noreply.github.com>
2023-02-02 17:54:46 -0600

    Create Fibonacci_Sequence.cpp

---
+++ C++_HackerRank/Fibonacci_Sequence.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void test(int range)
+{
+    cout << 0;
+
+    int fibonacci = 1;
+    int temp = 0;
+
+    for (int i = 1; i < range; i++) {
+        cout << " " << fibonacci;
+        fibonacci += temp;
+        temp = fibonacci - temp;
+    }
+        
+}
