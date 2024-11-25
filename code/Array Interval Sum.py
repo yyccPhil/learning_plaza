02d7b0392e6d3d7f9dd14f33e94b061c430a2a38
Phil Yuan <yyccphil@gmail.com>
2024-11-24 18:52:22 -0600

    used sys.stdin.read to correctly read input in one time

--- code/Array Interval Sum.py
+++ code/Array Interval Sum.py
@@ -1,16 +1,31 @@
-nums_len = int(input())
-nums = list()
-for i in range(nums_len):
-    nums.append(int(input()))
+import sys
+input = sys.stdin.read
 
-start, end = map(int, input().split())
+def main():
 
-def intervalSum(nums, start, end):
-    for i in range(len(nums)-1):
+    input_cnt = 0
+    input_data = input().split()
+
+    nums_len = int(input_data[0])
+    input_cnt += 1
+
+    nums = list()
+    for i in range(input_cnt, input_cnt + nums_len):
+        nums.append(int(input_data[i]))
+
+    input_cnt += nums_len
+
+    for i in range(nums_len-1):
         nums[i+1] += nums[i]
-    if start > 0:
-        return nums[end] - nums[start - 1]
-    else:
-        return nums[end]
 
-print(intervalSum(nums,start,end))
+    for i in range(input_cnt,len(input_data),2):
+        start = int(input_data[i])
+        end = int(input_data[i+1])
+        if start > 0:
+            print(nums[end] - nums[start - 1])
+        else:
+            print(nums[end])
+
+if __name__ == "__main__":
+    main()
+
