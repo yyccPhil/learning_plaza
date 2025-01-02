367b913dce49c1638037b6aa5341d4d8ada63dca
Phil Yuan <yyccphil@gmail.com>
2025-01-02 12:14:33 -0600

    binery tree orderTraverse * 3

---
+++ code/144.Binary Tree Preorder Traversal.py
@@ -0,0 +1,53 @@
+#
+# @lc app=leetcode id=144 lang=python3
+# @lcpr version=20004
+#
+# [144] Binary Tree Preorder Traversal
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = list()
+        def dfs(root):
+            if root is None:
+                return
+            res.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        return res            
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [1,null,2,3]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1,2,3,4,5,null,8,null,null,6,7,9]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# []\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1]\n
+# @lcpr case=end
+
+#
+
---
+++ code/94.Binary Tree Inorder Traversal.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode id=94 lang=python3
+# @lcpr version=20004
+#
+# [94] Binary Tree Inorder Traversal
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = list()
+        def dfs(root):
+            if root is None:
+                return
+            
+            dfs(root.left)
+            res.append(root.val)
+            dfs(root.right)
+        
+        dfs(root)
+        return res
+            
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [1,null,2,3]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1,2,3,4,5,null,8,null,null,6,7,9]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# []\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1]\n
+# @lcpr case=end
+
+#
+
